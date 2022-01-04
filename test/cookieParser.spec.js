55157e1d326df6ee0319321cce8b4154e6eb00aa
Welberty Martins <38479002+welbertymartins@users.noreply.github.com>
2022-01-03 21:17:10 -0300

    Update cookieParser.spec.js

--- test/cookieParser.spec.js
+++ test/cookieParser.spec.js
@@ -12,7 +12,6 @@ describe("Cookie Parser", function() {
         const cookies = cookieParser(fakeCookieList)
         expect(cookies[0].key).to.equal('fizz')
         expect(cookies[0].value).to.equal('1')
-
         expect(cookies[1].key).to.equal('buzz')
         expect(cookies[1].value).to.equal('2')
     })
