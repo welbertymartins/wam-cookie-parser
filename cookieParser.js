825f02444b8a57a97c3f19b19f17228cc0c14daf
Welberty Martins <38479002+welbertymartins@users.noreply.github.com>
2022-01-03 21:14:41 -0300

    feat: v1.0.0

--- cookieParser.js
+++ cookieParser.js
@@ -1,3 +1,11 @@
 const cookieParser = (list = []) => {
-
+    const cookies = list.map((item) => {
+        const elems = item.split('=')
+        const key = elems[0] ?? ''
+        const value = elems[1] ?? ''
+        return { key, value }
+    })
+    return cookies
 }
+
+module.exports = cookieParser;
---
+++ test/cookieParser.spec.js
@@ -0,0 +1,20 @@
+const expect = require("chai").expect
+const cookieParser = require("../cookieParser")
+
+const fakeCookieList = [
+    "fizz=1",
+    "buzz=2"
+]
+
+describe("Cookie Parser", function() {
+    
+    it("Get Cookies", function () {
+        const cookies = cookieParser(fakeCookieList)
+        expect(cookies[0].key).to.equal('fizz')
+        expect(cookies[0].value).to.equal('1')
+
+        expect(cookies[1].key).to.equal('buzz')
+        expect(cookies[1].value).to.equal('2')
+    })
+
+})
