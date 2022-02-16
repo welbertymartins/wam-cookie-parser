e3ccea2cdac903ef425767d758eafbb932f95dec
Welberty Martins <38479002+welbertymartins@users.noreply.github.com>
2022-02-16 00:22:56 -0300

    Update cookieParser.js

--- cookieParser.js
+++ cookieParser.js
@@ -1,8 +1,8 @@
 const cookieParser = (list = []) => {
     const cookies = list.map((item) => {
-        const elems = item.split('=')
-        const key = elems[0] ?? ''
-        const value = elems[1] ?? ''
+        const elems = item.split("=")
+        const key = elems[0] ?? ""
+        const value = elems[1] ?? ""
         return { key, value }
     })
     return cookies
